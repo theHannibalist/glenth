f702615ac8c722b7e4bb137cde2fa05569ea1c7e
Hannibal <lenesis@proton.me>
2024-12-19 14:51:58 +0330

    Support all formats

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ print ("Get Length for Linux, based on ffmeg, Developed by Lenesis")
 
 from os import system as term
 
-term('ls *.mp4 | xargs -I file ffprobe -i file -show_entries format=duration -v quiet -of csv="p=0" > list.tmp')
-with open('list.tmp') as f:
+term('ls *.mp4 *.mov *.avi *.mkv *.mpg *.mpeg 2> /dev/null 2> /dev/null | xargs -I file ffprobe -i file -show_entries format=duration -v quiet -of csv="p=0" > .list.tmp')
+with open('.list.tmp') as f:
     i = 0
     for line in f:
         i += float(line)
@@ -13,5 +13,5 @@ with open('list.tmp') as f:
     hr = int(float(i / 60 / 60))
     mn = int(( i /60) % 60)
     print (f"total {hr} hours and {mn} minutes")
-term ('rm list.tmp')
+term ('rm .list.tmp')
 
