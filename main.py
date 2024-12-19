e79ffd6cab89aba41a60be43527733fdfcc6ce70
Hannibal <lenesis@proton.me>
2024-12-19 10:07:19 +0330

    Exact Hour and Minutes

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+print ("Get Length for Linux, based on ffmeg, Developed by Lenesis")
+
 from os import system as term
 
 term('ls *.mp4 | xargs -I file ffprobe -i file -show_entries format=duration -v quiet -of csv="p=0" > list.tmp')
@@ -6,6 +10,8 @@ with open('list.tmp') as f:
     for line in f:
         i += float(line)
 
-    print(int(i / 60 / 60),'h')
+    hr = int(float(i / 60 / 60))
+    mn = int(( i /60) % 60)
+    print (f"total {hr} hours and {mn} minutes")
 term ('rm list.tmp')
 
