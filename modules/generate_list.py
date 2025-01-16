063ae239883602f83a9e892bf2fdb17d1ea6f449
Hannibal <lenesis@proton.me>
2025-01-16 08:18:36 +0330

    ls only

--- modules/generate_list.py
+++ modules/generate_list.py
@@ -3,6 +3,6 @@ from os import system as term
 def scan(target, type, all):
     target = f'"{target}"'
     if all:
-        term(f'ls {target}/*.mp4 {target}/*.mov {target}/*.avi {target}/*.mkv {target}/*.mpg {target}/*.mpeg 2> /dev/null | xargs -I file ffprobe -i file -show_entries format=duration -v quiet -of csv="p=0" > .list.tmp')
+        term(f'ls {target}/*.mp4 {target}/*.mov {target}/*.avi {target}/*.mkv {target}/*.mpg {target}/*.mpeg 2> /dev/null | echo > .lst.tmp')
     else:
-        term(f'ls {target}/*.{type} 2> /dev/null | xargs -I file ffprobe -i file -show_entries format=duration -v quiet -of csv="p=0" > .list.tmp')
+        term(f'ls {target}/*.{type} 2> /dev/null | echo > .lst.tmp')
