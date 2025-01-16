75a3ea28ee2e1f4dac19e3dbc225b9c313e8c2a6
Hannibal <lenesis@proton.me>
2025-01-16 09:12:44 +0330

    listing system

--- modules/generate_list.py
+++ modules/generate_list.py
@@ -3,6 +3,6 @@ from os import system as term
 def scan(target, type, all):
     target = f'"{target}"'
     if all:
-        term(f'ls {target}/*.mp4 {target}/*.mov {target}/*.avi {target}/*.mkv {target}/*.mpg {target}/*.mpeg 2> /dev/null | echo > .lst.tmp')
+        term(f'ls {target}/*.mp4 {target}/*.mov {target}/*.avi {target}/*.mkv {target}/*.mpg {target}/*.mpeg 2> /dev/null | cp /dev/stdin .lst.tmp')
     else:
-        term(f'ls {target}/*.{type} 2> /dev/null | echo > .lst.tmp')
+        term(f'ls {target}/*.{type} 2> /dev/null | cp /dev/stdin .lst.tmp')
--- modules/get_length.py
+++ modules/get_length.py
@@ -1,12 +1,19 @@
 from os import system as term
-
+from modules import conv
 def show_final():
-    with open('.list.tmp') as f:
+    with open('.lst.tmp') as f:
         i = 0
         for line in f:
-            i += float(line)
+            # print (line.strip())
+            for item in line.split():
+                term (f'ffprobe -i {item} -show_entries format=duration -v quiet -of csv="p=0" >> .timelist.tmp')
+                with open ('.timelist.tmp') as t:
+                    for time in t :
+                        i += float(time)
+                        print (f'\033[33m*\033[39m {item} ===> \033[33m{conv.ert(time)['hr']} hours \033[39mand \033[33m{conv.ert(time)['mn']}\033[39m\n')
+        
+            
 
-        hr = int(float(i / 60 / 60))
-        mn = int(( i /60) % 60)
-        print (f"total\033[33m {hr} hours\033[39m and\033[33m {mn} minutes\033[39m")
-        term ('rm .list.tmp')
+
+        print (f"total:\n\033[33m {conv.ert(i)['hr']} hours\033[39m and\033[33m {conv.ert(i)['mn']} minutes\033[39m")
+        term ('rm .lst.tmp .timelist.tmp')
