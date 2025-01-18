8ac3cf0553e32b1e4831f68a1a81c27e465f2b92
Hannibal <lenesis@proton.me>
2025-01-18 18:17:48 +0330

    arg update

--- modules/cli_system.py
+++ modules/cli_system.py
@@ -8,6 +8,7 @@ def run_parser() -> dict:
 
     parser.add_argument ('-t', '--type', default='all', choices=['mpg', 'mpeg', 'mp4', 'mov', 'mkv', 'all'], help='Choose specific file type (default : all)')
     parser.add_argument ('-d', '--target-dir', default='.', help='choose a directory (default : current directory)')
+    parser.add_argument ('--sub-directories' , default=False , action='store_true', help='scan sub-directories for videos as well')
 
     args = parser.parse_args()
     all = True
