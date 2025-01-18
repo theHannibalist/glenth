74ed15bd4025bae409fd8ea760c005229a31c02f
Hannibal <lenesis@proton.me>
2025-01-18 19:47:12 +0330

    collect time done

--- modules/generate_list.py
+++ modules/generate_list.py
@@ -2,15 +2,15 @@ from os import system as term
 
 def scan(target, type, all, subs):
     if subs:
-            term ('ls -d1 */ > .dirlist.tmp')
-            with open ('.dirlist.tmp') as dls:
-                for dir in dls:
-                    dir = dir.strip()
-                    if all:
-                        term(f'ls "{target}/{dir}" | grep -E "\\.mp4|\\.mkv|\\.mov|\\.mpg|\\.mpeg|\\.avi" | cat /dev/stdin | xargs -I filename echo "{dir}"filename >> .lst.tmp')
-                    else:
-                        term(f'ls -R {target} | grep -E "\\.{type}" 2> /dev/null | cp /dev/stdin .lst.tmp')
-                exit()
+        term ('ls -d1 */ > .dirlist.tmp')
+        with open ('.dirlist.tmp') as dls:
+            for dir in dls:
+                dir = dir.strip()
+                if all:
+                    term(f'ls "{target}/{dir}" | grep -E "\\.mp4|\\.mkv|\\.mov|\\.mpg|\\.mpeg|\\.avi" | cat /dev/stdin | xargs -I filename echo "{dir}"filename >> .lst.tmp')
+                else:
+                    term(f'ls -R {target} | grep -E "\\.{type}" 2> /dev/null | cp /dev/stdin .lst.tmp')
+        # exit()
     else:
         target = f"'{target}'"
         if all:
--- modules/get_length.py
+++ modules/get_length.py
@@ -4,16 +4,24 @@ def show_final():
     with open('.lst.tmp') as f:
         i = 0
         for line in f:
-            # print (line.strip())
-            for item in line.split():
-                term (f'ffprobe -i {item} -show_entries format=duration -v quiet -of csv="p=0" > .timelist.tmp')
-                with open ('.timelist.tmp') as t:
-                    for time in t :
-                        i += float(time)
-                        print (f'\033[33m*\033[39m {item} ===> \033[33m{conv.ert(time)['hr']} hours \033[39mand \033[33m{conv.ert(time)['mn']} minutes\033[39m\n')
+            line = line.strip()
+            term (f'ffprobe -i "{line}" -show_entries format=duration -v quiet -of csv="p=0" > .timelist.tmp')
+            with open ('.timelist.tmp') as t:
+                for time in t :
+                    i += float(time)
+                    print (f'\033[33m*\033[39m {line} ===> \033[33m{conv.ert(time)['hr']} hours \033[39mand \033[33m{conv.ert(time)['mn']} minutes\033[39m\n')
+        
+            # for item in line.split():
+                # print (item)
+                # exit()
+                # term (f'ffprobe -i {item} -show_entries format=duration -v quiet -of csv="p=0" > .timelist.tmp')
+                # with open ('.timelist.tmp') as t:
+                #     for time in t :
+                #         i += float(time)
+                #         print (f'\033[33m*\033[39m {item} ===> \033[33m{conv.ert(time)['hr']} hours \033[39mand \033[33m{conv.ert(time)['mn']} minutes\033[39m\n')
         
             
 
 
         print (f"total:\n\033[32m {conv.ert(i)['hr']} hours\033[39m and\033[32m {conv.ert(i)['mn']} minutes\033[39m")
-        term ('rm .lst.tmp .timelist.tmp')
+        term ('rm .lst.tmp .timelist.tmp .dirlist.tmp 2> /dev/null')
